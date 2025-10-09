c6e0a674e17127978f03c547322714c3d6ba9243
wlxlatiao <wlxlatiao@163.com>
2025-10-09 23:11:06 +0800

    modified metrics.py

--- metrics.py
+++ metrics.py
@@ -49,9 +49,16 @@ if __name__ == "__main__":
         if dataset == "gsm8k":
             gt_answer = parse_gsm8k_answer(original_data["answer"])
         elif dataset == "reclor":
-            gt_answer = original_data["answer"]
+            label = original_data["label"]
+            label_to_answer = {
+                0: "A",
+                1: "B",
+                2: "C",
+                3: "D",
+            }
+            gt_answer = label_to_answer[label]
         else:
-            gt_answer = original_data["answer"]
+            gt_answer = 'No answer.'
 
         probs = item["probs"]
         entropies = item["entropies"]
