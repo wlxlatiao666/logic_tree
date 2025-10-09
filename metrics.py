045bfa3ac03da44832e90f0c37c8b0b675578869
wlxlatiao <wlxlatiao@163.com>
2025-10-09 23:07:32 +0800

    add metrics.py

---
+++ metrics.py
@@ -0,0 +1,121 @@
+import json
+import argparse
+import math
+import time
+from typing import List
+from sentence_transformers import SentenceTransformer, util
+from answer_parser import parse_model_answer, parse_gsm8k_answer
+
+embedder = SentenceTransformer('/mnt/public/gpfs-jd/code/weilongxuan/all-mpnet-base-v2')
+
+def calculate_diversity(texts: List[str]):
+    if len(texts) < 2:
+        return 0.0
+
+    distances = []
+    embeddings = []
+    for text in texts:
+        embeddings.append(embedder.encode(text, convert_to_tensor=True))
+
+    for i in range(len(texts)):
+        current_distances = []
+        for j in range(len(texts)):
+            if i != j:
+                cos_sim = util.cos_sim(embeddings[i], embeddings[j])
+                current_distances.append(1 - cos_sim)
+        distances.append(sum(current_distances) / len(current_distances))
+    
+    diversity = sum(distances)
+    return diversity.item()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, required=True)
+    parser.add_argument("--file_name", type=str, required=True, help="Don't include .json suffix")
+    args = parser.parse_args()
+
+    dataset = args.dataset
+    file_name = args.file_name
+    # dataset_path = f"./data/{dataset}/test.json"
+    input_path = f'./results/{dataset}/{file_name}.json'
+    output_path = f'./results/{dataset}/{file_name}_metrics.json'
+
+    with open(input_path, 'r') as f:
+        data = json.load(f)
+
+    results = []
+    for i, item in enumerate(data):
+        original_data = item['original_data']
+        if dataset == "gsm8k":
+            gt_answer = parse_gsm8k_answer(original_data["answer"])
+        elif dataset == "reclor":
+            gt_answer = original_data["answer"]
+        else:
+            gt_answer = original_data["answer"]
+
+        probs = item["probs"]
+        entropies = item["entropies"]
+        texts = item["texts"]
+
+        uncertainties = {}
+        labels = {}
+
+        # calculate uncertainty metrics
+        uncertainties["weighted_leaf_avg_entropy"] = sum(p * e for p, e in zip(probs, entropies))
+        max_prob_index = probs.index(max(probs))
+        uncertainties["max_leaf_avg_entropy"] = entropies[max_prob_index]
+        uncertainties["avg_leaf_avg_entropy"] = sum(e for e in entropies) / len(entropies)
+        uncertainties["complexity"] = item["complexity"]
+
+        answer_buckets = {}
+        for i, text in enumerate(texts):
+            parsed_answer = parse_model_answer(text)
+            if parsed_answer:
+                try: 
+                    key = parsed_answer
+                    answer_buckets[parsed_answer] = answer_buckets.get(parsed_answer, 0.0) + probs[i]
+                except:
+                    continue
+        total = sum(answer_buckets.values())
+        pe = 0.0
+        if total > 0:
+            probabilities = [p / total for p in answer_buckets.values()]
+            pe = -sum(p * math.log(p) for p in probabilities if p > 0)
+        uncertainties["predictive_entropy_weighted"] = float(pe)
+
+        probs_entropy = -sum(prob * math.log(prob) for prob in probs if prob > 0)
+        uncertainties["probs_entropy"] = float(probs_entropy)
+        
+        # calculate labels
+        final_answer_vote_weighted = max(answer_buckets.items(), key=lambda x: x[1]) if answer_buckets else ""
+        final_answer_vote_weighted_nothreshold = final_answer_vote_weighted[0]
+        labels["answer_vote_weighted_nothreshold"] = int(final_answer_vote_weighted_nothreshold == gt_answer) if gt_answer else 0
+
+        final_answer_vote_weighted_threshold = final_answer_vote_weighted[0] if final_answer_vote_weighted[1] > 0.5 else ""
+        labels["answer_vote_weighted_threshold"] = int(final_answer_vote_weighted_threshold == gt_answer) if gt_answer else 0
+
+        min_entropy_index = entropies.index(min(entropies))
+        final_answer_min_entropy = parse_model_answer(texts[min_entropy_index])
+        labels["answer_min_entropy"] = int(final_answer_min_entropy == gt_answer) if gt_answer else 0
+            
+        passk = False
+        for answer in answer_buckets:
+            if answer == gt_answer:
+                passk = True
+                break
+        labels["pass@k"] = int(passk)
+
+        diversity = calculate_diversity(texts)
+
+        results.append({
+            "gt_answer": gt_answer,
+            "diversity": diversity,
+            "label": labels,
+            "uncertainty": uncertainties
+        })
+        print(f"Processed {i+1} items")
+
+    with open(output_path, 'w') as f:
+        json.dump(results, f, indent=2)
+        
+    print(f"\nResults saved to {output_path}")
