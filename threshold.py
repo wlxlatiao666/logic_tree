6660d3d2ad9a23c6d1b9151c5a398a86731db271
wlxlatiao666 <wlxlatiao@163.com>
2025-10-15 11:19:48 +0000

    10.15

---
+++ threshold.py
@@ -0,0 +1,7 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, required=True)
+    args = parser.parse_args()
+    dataset = args.dataset
