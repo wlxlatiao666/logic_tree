9c6bb7e8c2dff25fa389ce2d1dd9822a1ab34fdc
wlxlatiao666 <wlxlatiao@163.com>
2025-10-28 08:21:53 +0000

    10.28

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python generate_multi.py --dataset reclor --samples 50 --test_size 3
-python generate_multi.py --dataset gsm8k --samples 50 --test_size 3
 python generate_multi.py --dataset reclor --samples 5
+python generate_multi.py --dataset reclor --samples 100 --test_size 20
+python generate_multi.py --dataset reclor --samples 1000
