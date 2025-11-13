dbc000fdb485c9d7b1589153904abd92626c394e
wlxlatiao666 <wlxlatiao@163.com>
2025-11-13 13:19:38 +0000

    11.13

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 python generate.py --dataset gpqa --test_size 100 --sample True
+python generate.py --dataset gpqa --test_size 100 --sample True
+python generate.py --dataset gpqa --test_size 100 --sample True
