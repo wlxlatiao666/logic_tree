12733d7f5824d28bd58d4ca0a26113d3d108c0e4
wlxlatiao666 <wlxlatiao@163.com>
2025-12-01 13:09:06 +0000

    12.1

--- run_qwen2.5_7B.sh
+++ run_qwen2.5_7B.sh
@@ -1,6 +1,8 @@
-python generate.py --model Qwen2.5-7B-Instruct --dataset math500 --num_leaves 20
-python generate.py --model Qwen2.5-7B-Instruct --dataset aime24 --num_leaves 20
-python generate.py --model Qwen2.5-7B-Instruct --dataset aime25 --num_leaves 20
-python generate.py --model Qwen2.5-7B-Instruct --dataset gpqa-main --num_leaves 20
-python generate.py --model Qwen2.5-7B-Instruct --dataset gpqa-diamond --num_leaves 20
-
+python generate_ddp.py --model Qwen2.5-7B-Instruct --dataset aime24
+python generate_ddp.py --model Qwen2.5-7B-Instruct --dataset aime25
+python generate_ddp.py --model Qwen2.5-7B-Instruct --dataset math500
+python generate_ddp.py --model Qwen2.5-7B-Instruct --dataset gpqa-diamond
+python generate_ddp.py --model Qwen2.5-7B-Instruct --dataset gpqa-main
+python generate_ddp.py --model Qwen2.5-7B-Instruct --dataset csqa
+python generate_ddp.py --model Qwen2.5-7B-Instruct --dataset arc-e
+python generate_ddp.py --model Qwen2.5-7B-Instruct --dataset arc-c
