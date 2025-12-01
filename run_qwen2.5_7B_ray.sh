d22453afd2b58d081179540e0b63fbf5602695d4
wlxlatiao666 <wlxlatiao@163.com>
2025-12-01 11:22:38 +0000

    12.1

--- run_qwen2.5_7B_ray.sh
+++ run_qwen2.5_7B_ray.sh
@@ -1,8 +1,8 @@
-python generate_multi_vllm_ray.py --model Qwen2.5-7B --dataset math500 --samples 20 --num_gpus 8
-python generate_multi_vllm_ray.py --model Qwen2.5-7B --dataset gpqa-diamond --samples 20 --num_gpus 8
-python generate_multi_vllm_ray.py --model Qwen2.5-7B --dataset gpqa-main --samples 20 --num_gpus 8
-python generate_multi_vllm_ray.py --model Qwen2.5-7B --dataset csqa --samples 20 --num_gpus 8
-python generate_multi_vllm_ray.py --model Qwen2.5-7B --dataset arc-e --samples 20 --num_gpus 8
-python generate_multi_vllm_ray.py --model Qwen2.5-7B --dataset arc-c --samples 20 --num_gpus 8
-python generate_multi_vllm_ray.py --model Qwen2.5-7B --dataset aime24 --samples 20 --num_gpus 8
-python generate_multi_vllm_ray.py --model Qwen2.5-7B --dataset aime25 --samples 20 --num_gpus 8
+python generate_multi_vllm_ray.py --model Qwen2.5-7B-Instruct --dataset math500 --samples 20 --num_gpus 8
+python generate_multi_vllm_ray.py --model Qwen2.5-7B-Instruct --dataset gpqa-diamond --samples 20 --num_gpus 8
+python generate_multi_vllm_ray.py --model Qwen2.5-7B-Instruct --dataset gpqa-main --samples 20 --num_gpus 8
+python generate_multi_vllm_ray.py --model Qwen2.5-7B-Instruct --dataset csqa --samples 20 --num_gpus 8
+python generate_multi_vllm_ray.py --model Qwen2.5-7B-Instruct --dataset arc-e --samples 20 --num_gpus 8
+python generate_multi_vllm_ray.py --model Qwen2.5-7B-Instruct --dataset arc-c --samples 20 --num_gpus 8
+python generate_multi_vllm_ray.py --model Qwen2.5-7B-Instruct --dataset aime24 --samples 20 --num_gpus 8
+python generate_multi_vllm_ray.py --model Qwen2.5-7B-Instruct --dataset aime25 --samples 20 --num_gpus 8
