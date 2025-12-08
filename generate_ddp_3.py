2764133f01469d335b9b752a176b867fdc0f8c8a
wlxlatiao666 <wlxlatiao@163.com>
2025-12-08 14:07:11 +0000

    update scripts

--- generate_ddp_3.py
+++ generate_ddp_3.py
@@ -9,7 +9,7 @@ import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from logic_tree_decode_2 import logic_branch_decode
+from logic_tree_decode_3 import logic_branch_decode
 from utils import generate_usr_prompt
 from threshold import get_threshold
 
@@ -180,9 +180,9 @@ def merge_results(args):
     
     # Save final results
     if test_size == -1:
-        output_path = f"./results/{model_name}/{dataset}/logic_tree_results_all_leaves{num_leaves}_threshold{tau}_ddp_0.2_2json"
+        output_path = f"./results/{model_name}/{dataset}/logic_tree_results_all_leaves{num_leaves}_threshold{tau}_ddp_random.json"
     else:
-        output_path = f"./results/{model_name}/{dataset}/logic_tree_results_{test_size}_leaves{num_leaves}_threshold{tau}_ddp_0.2_2.json"
+        output_path = f"./results/{model_name}/{dataset}/logic_tree_results_{test_size}_leaves{num_leaves}_threshold{tau}_ddp_random.json"
     
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     with open(output_path, 'w', encoding="utf8") as f:
--- run_qwen2.5_7B.sh
+++ run_qwen2.5_7B.sh
@@ -1,3 +1,3 @@
-python generate_ddp.py --model Qwen2.5-7B-Instruct --dataset math500 --tau 95
+python generate_ddp.py --model Qwen2.5-7B-Instruct --dataset math500 --tau 96
 python generate_ddp_2.py --model Qwen2.5-7B-Instruct --dataset math500 
-
+python generate_ddp_3.py --model Qwen2.5-7B-Instruct --dataset math500
