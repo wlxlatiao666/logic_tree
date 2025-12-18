5ec0cd3361f9b1a663d932d02872ac28f3d2bf56
wlxlatiao666 <wlxlatiao@163.com>
2025-12-18 16:27:49 +0000

    modified test version

--- 2.sh
+++ 2.sh
@@ -1,2 +1,4 @@
-python metrics.py --model Qwen2.5-7B-Instruct --dataset gpqa-main --file_name logic_tree_results_all_leaves20_threshold80_ddp_relevance.json
-python evaluate.py --model Qwen2.5-7B-Instruct --dataset gpqa-main --sc_file generated_answers_20samples_ray_8gpus.json --lt_file logic_tree_results_all_leaves20_threshold80_ddp_relevance_metrics.json
+python metrics.py --model Qwen2.5-7B-Instruct --dataset aime24 --file_name logic_tree_results_all_leaves20_threshold80_ddp_relevance.json
+python evaluate.py --model Qwen2.5-7B-Instruct --dataset aime24 --sc_file generated_answers_20samples_ray_8gpus.json --lt_file logic_tree_results_all_leaves20_threshold80_ddp_relevance_metrics.json
+python metrics.py --model Qwen2.5-7B-Instruct --dataset aime25 --file_name logic_tree_results_all_leaves20_threshold80_ddp_relevance.json
+python evaluate.py --model Qwen2.5-7B-Instruct --dataset aime25 --sc_file generated_answers_20samples_ray_8gpus.json --lt_file logic_tree_results_all_leaves20_threshold80_ddp_relevance_metrics.json
